0af03f88eaa7a939f5a3e6e4cd939e5b6a06c5b9
venom202 <30728692+venom202@users.noreply.github.com>
2017-08-13 14:32:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ app.get('/article-one',function(req,res) {
 });
 
 app.get('/article-two',function(req,res) {
-    res.send('Article two is requested and will be provided')
+    res.send('Article two is requested and will be provided');
 });
 
 app.get('/article-three',function(req,res) {
-    res.send('Article three is requested and will be provided')
+    res.send('Article three is requested and will be provided');
 });
 
 
