aa6ef906e714b82597ff29720e326dac4fe2ce5f
venom202 <30728692+venom202@users.noreply.github.com>
2017-08-17 23:28:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,14 +83,14 @@ var htmlTemplate=`
 return htmlTemplate;
 }
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
  
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
+*/
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
