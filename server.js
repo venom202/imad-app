8453baa6e9e3408b4bf8b21646885292227ae09d
venom202 <30728692+venom202@users.noreply.github.com>
2017-08-17 23:29:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,17 +83,17 @@ var htmlTemplate=`
 return htmlTemplate;
 }
 
-/*app.get('/', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
  
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-*/
-app.get('/ui/madi.png', function (req, res) {
+
+/*app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
+});*/
 
 app.get('/:articleName',function(req,res) {
     //articleName==article-one
@@ -102,14 +102,15 @@ app.get('/:articleName',function(req,res) {
     res.send(createTemplate(articles[articleName]));
 });
 
-/*app.get('/article-two',function(req,res) {
+/*
+app.get('/article-two',function(req,res) {
     res.sendFile(path.join(__dirname,'ui','article-two.html'));
 });
 
 app.get('/article-three',function(req,res) {
     res.sendFile(path.join(__dirname,'ui','article-three.html'));
-});*/
-
+});
+*/
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
