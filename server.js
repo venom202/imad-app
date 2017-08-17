72c9875663f71a50c7938bb0255b3c10c65cfc57
venom202 <30728692+venom202@users.noreply.github.com>
2017-08-17 23:07:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne={                                                  
+articleOne={                                                  
     title:`Article One`,
     heading:`Article One`,
     date:`Apr 18,2017`,
