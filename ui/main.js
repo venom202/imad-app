7b3f2c4c4eea656069878d87420bfd77aee339e3
venom202 <30728692+venom202@users.noreply.github.com>
2017-08-18 00:18:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,6 @@ console.log('Loaded!');
 
 //move madi image code
 var image=document.getElementById('madi');
-img.onclick=function(){
-  madi.style.marginleft='100px';  
+image.onclick=function(){
+  image.style.marginleft='100px';  
 };
