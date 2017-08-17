c4387a7d5f1fc7ca96b64c5f50876779fffea45e
venom202 <30728692+venom202@users.noreply.github.com>
2017-08-17 23:32:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,6 +89,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
  
+ app.get('/:articleName',function(req,res) {
+    //articleName==article-one
+    //articles[articleName]==content object for article one
+    var articleName=req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -97,12 +104,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/:articleName',function(req,res) {
-    //articleName==article-one
-    //articles[articleName]==content object for article one
-    var articleName=req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
-});
+
 
 /*
 app.get('/article-two',function(req,res) {
