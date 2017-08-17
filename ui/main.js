882a35727f2554cc65d171275671ac7f8bd5957e
venom202 <30728692+venom202@users.noreply.github.com>
2017-08-18 00:56:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,20 @@
 console.log('Loaded!');
 
+/*
 //move madi image onclick to left
 var img=document.getElementById('madi');
 img.onclick=function(){
   img.style.marginLeft='100px';  
 };
+*/
+
+//move madi image continously
+var img=getElementById('madi');
+var marginLeft=0;                                //variable
+function moveimg(){                           
+    marginLeft=marginLeft + 1;
+    img.style.marginLeft + 'px';
+}
+img.onclick=function(){
+    var interval=setInterval(moveimg,50);         //apply moveimg function every 100ms
+}
