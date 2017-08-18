4e55efc3bd6a1c569c30229f3e4c3800a5a44a78
venom202 <30728692+venom202@users.noreply.github.com>
2017-08-18 12:27:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -129,7 +129,7 @@ app.get('/article-three',function(req,res) {
 */
 
 var counter=0;
-app.get('/counter',function(req, res){
+app.get('/counter',function(req, res) {
     counter=counter + 1;
     res.send(counter.toString());
 });
