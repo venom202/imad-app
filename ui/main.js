0b05de76a6b20939b0c0710f326a4cb8e20265f8
venom202 <30728692+venom202@users.noreply.github.com>
2017-08-19 23:22:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,3 +43,20 @@ button.onclick=function() {
     request.open('GET','http://u202mehulpatil.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+
+//display name code
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function() {
+    //make a request to server and send name
+    //capture name and render it as list
+    var name=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i=name.length;i++) {
+        list += '<li>' + name[i] + '</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML.list;
+};
