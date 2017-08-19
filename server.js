21ed272b81fd4a4b166da329df84d53e7eb02743
venom202 <30728692+venom202@users.noreply.github.com>
2017-08-20 00:01:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,9 +119,9 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-/*app.get('/ui/main.js', function (req, res) {                         //this code is added to run client side javascript for alert box
+app.get('/ui/main.js', function (req, res) {                         
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});*/
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
