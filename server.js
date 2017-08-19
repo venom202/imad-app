1a2ad68801798b429dd3a02b32f09ed78e899661
venom202 <30728692+venom202@users.noreply.github.com>
2017-08-19 23:37:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names[];    
+var names=[];    
 app.get('/submit-name/:name', function(req, res){                      //  /:name is passed as URL object
     //get the name from request
     var name=req.param.name;
